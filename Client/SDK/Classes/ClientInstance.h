75df1e86d340c002d8cae84695553b558f0e00b2
OvermindMC <Overmind@mail.com>
2023-07-17 04:04:52 +0100

    Update ClientInstance.h

--- Client/SDK/Classes/ClientInstance.h
+++ Client/SDK/Classes/ClientInstance.h
@@ -8,6 +8,8 @@ public:
     unsigned long long** VTable;
 public:
     auto getPlayer(void) -> Player*;
+public:
+    auto getbadrefdef(void) -> glmatrixf*;
 };
 
 #endif /* CLIENT_SDK_CLASSES_CLIENTINSTANCE */
