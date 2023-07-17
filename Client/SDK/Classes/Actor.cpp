486b395350f85a22650edce2a53e3783613546b3
OvermindMC <Overmind@mail.com>
2023-07-17 16:10:05 +0100

    Create Actor.cpp

---
+++ Client/SDK/Classes/Actor.cpp
@@ -0,0 +1,14 @@
+#include "Actor.h"
+#include "../../Mem/Mem.h"
+
+auto Actor::getRuntimeID(void) -> uint64_t {
+
+    using GetRuntimeIDComp = RuntimeIDComponent* (__thiscall*)(void*, EntityId*);
+    static auto _GetRuntimeIDComp = (GetRuntimeIDComp)(Mem::findSig("40 53 48 83 EC ? 48 8B DA BA 14 14 ? ?"));
+
+    auto registryBase = *(void**)(this->ctx.registry);
+    auto runtimeID = _GetRuntimeIDComp(registryBase, &this->ctx.id);
+
+    return runtimeID->runtimeId;
+
+};
