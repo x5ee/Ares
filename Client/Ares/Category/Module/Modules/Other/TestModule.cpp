cb072622ee6957a7ad49f58ca0e88d5b08c1d723
OvermindMC <Overmind@mail.com>
2023-07-18 20:18:15 +0100

    Update TestModule.cpp

--- Client/Ares/Category/Module/Modules/Other/TestModule.cpp
+++ Client/Ares/Category/Module/Modules/Other/TestModule.cpp
@@ -1,23 +1,5 @@
 #include "TestModule.h"
 
-auto TestModule::onImGui(void) -> void {
-
-    auto categories = this->category->mgr->categories;
-    
-    if(ImGui::Begin("ImGui")) {
-
-        for(auto category : categories) {
-
-            //
-
-        };
-        
-        ImGui::End();
-
-    };
-
-};
-
 auto TestModule::onGameMode(GameMode* GM) -> void {
 
     GM->player->setSprinting(true);
