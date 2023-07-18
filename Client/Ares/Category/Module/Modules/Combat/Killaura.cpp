679e101e599ca8966d8ee98f89b0464ec82ef0fa
OvermindMC <Overmind@mail.com>
2023-07-18 08:39:22 +0100

    Create Killaura.cpp

---
+++ Client/Ares/Category/Module/Modules/Combat/Killaura.cpp
@@ -0,0 +1,59 @@
+#include "Killaura.h"
+
+auto Killaura::onGameMode(GameMode* GM) -> void {
+
+    auto player = GM->player;
+    auto level = player->getLevel();
+    auto myPos = *player->getPosition();
+    auto localRuntimeID = player->getRuntimeID();
+    auto entMap = this->category->mgr->entityMap;
+
+    if(entMap.empty())
+        return;
+    
+    auto dists = std::map<uint64_t, double>();
+    for(auto [ runtimeId, ent ] : entMap) {
+
+        if(!ent->isAlive() || localRuntimeID == runtimeId)
+            continue;
+        
+        auto typeId = ent->getEntityTypeId();
+
+        if(typeId == 64 || typeId == 69)
+            continue;
+        
+        auto dist = (*ent->getPosition()).dist(myPos);
+
+        if(dist <= this->range)
+            dists[runtimeId] = dist;
+
+    };
+
+    std::vector<std::pair<uint64_t, double>> distsVector(dists.begin(), dists.end());
+    
+    std::sort(distsVector.begin(), distsVector.end(),
+        [&](const std::pair<uint64_t, double>& a, const std::pair<uint64_t, double>& b) {
+            return a.second < b.second;
+        }
+    );
+
+    dists = std::map<uint64_t, double>(distsVector.begin(), distsVector.end());
+    
+    auto count = 0;
+    for(auto [ runtimeId, dist ] : dists) {
+
+        auto entity = level->getRuntimeEntity(runtimeId);
+
+        if(!entity)
+            continue;
+        
+        if(count >= 2)
+            break;
+        
+        count++;
+        player->swing();
+        GM->attack(entity);
+
+    };
+
+};
