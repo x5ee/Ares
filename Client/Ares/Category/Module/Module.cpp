db8e1be22528c8b1bc01ec0254f909c10358932c
OvermindMC <Overmind@mail.com>
2023-07-18 08:06:38 +0100

    Update Module.cpp

--- Client/Ares/Category/Module/Module.cpp
+++ Client/Ares/Category/Module/Module.cpp
@@ -5,6 +5,8 @@ Module::Module(std::string n, Category* c) {
     this->name = n.c_str();
     this->category = reinterpret_cast<Category*>(c);
 
+    c->registerModule(this);
+
 };
 
 auto Module::baseTick(void) -> void {
