e78b072a6a983471fc4704eecd54536a0b87b7d4
OvermindMC <Overmind@mail.com>
2023-07-18 07:58:13 +0100

    Create Module.h

---
+++ Client/Ares/Category/Module/Module.h
@@ -0,0 +1,22 @@
+#ifndef CLIENT_ARES_CATEGORY_MODULE_MODULE
+#define CLIENT_ARES_CATEGORY_MODULE_MODULE
+
+#include "../Category.h"
+
+class Module {
+public:
+    std::string name;
+    Category* category;
+public:
+    bool wasEnabled = false, isEnabled = false;
+public:
+    Module(std::string, Category*);
+public:
+    auto baseTick(void) -> void;
+public:
+    virtual auto onTick(void) -> void {};
+    virtual auto onEnable(void) -> void {};
+    virtual auto onDisable(void) -> void {};
+};
+
+#endif /* CLIENT_ARES_CATEGORY_MODULE_MODULE */
