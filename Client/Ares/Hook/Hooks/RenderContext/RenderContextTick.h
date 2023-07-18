2bf8c9e8e3654a208bdf9710d402898a8bdb1041
OvermindMC <Overmind@mail.com>
2023-07-18 08:52:09 +0100

    Create RenderContextTick.h

---
+++ Client/Ares/Hook/Hooks/RenderContext/RenderContextTick.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_ARES_HOOK_HOOKS_RENDERCONTEXT_RENDERCONTEXTTICK
+#define CLIENT_ARES_HOOK_HOOKS_RENDERCONTEXT_RENDERCONTEXTTICK
+
+#include "../../Hook.h"
+
+class RenderCtxHook : public Hook {
+public:
+    RenderCtxHook(Manager* mgr) : Hook(mgr) {
+        this->init();
+    };
+public:
+    auto init(void) -> void override;
+};
+
+#endif /* CLIENT_ARES_HOOK_HOOKS_RENDERCONTEXT_RENDERCONTEXTTICK */
