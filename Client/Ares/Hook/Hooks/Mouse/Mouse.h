24403806bfb3b530d98daaf83ef50eee73a68000
OvermindMC <Overmind@mail.com>
2023-07-18 14:58:04 +0100

    Create Mouse.h

---
+++ Client/Ares/Hook/Hooks/Mouse/Mouse.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_ARES_HOOK_HOOKS_MOUSE_MOUSE
+#define CLIENT_ARES_HOOK_HOOKS_MOUSE_MOUSE
+
+#include "../../Hook.h"
+
+class MouseHook : public Hook {
+public:
+    MouseHook(Manager* mgr) : Hook(mgr) {
+        this->init();
+    };
+public:
+    auto init(void) -> void override;
+};
+
+#endif /* CLIENT_ARES_HOOK_HOOKS_MOUSE_MOUSE */
