6c2ad927e9292385b591634f4fb3fbbe14133f08
OvermindMC <Overmind@mail.com>
2023-07-18 07:58:10 +0100

    Create Module.cpp

---
+++ Client/Ares/Category/Module/Module.cpp
@@ -0,0 +1,26 @@
+#include "Module.h"
+
+Module::Module(std::string n, Category* c) {
+
+    this->name = n.c_str();
+    this->category = reinterpret_cast<Category*>(c);
+
+};
+
+auto Module::baseTick(void) -> void {
+
+    if(wasEnabled != isEnabled) {
+
+        wasEnabled = isEnabled;
+
+        if(isEnabled)
+            onEnable();
+        else
+            onDisable();
+
+    };
+
+    if(isEnabled)
+        onTick();
+
+};
