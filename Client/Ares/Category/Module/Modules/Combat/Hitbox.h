84adb1f0049ea0e11337ead302a977c9dee0d8c3
OvermindMC <Overmind@mail.com>
2023-07-18 15:58:31 +0100

    Create Hitbox.h

---
+++ Client/Ares/Category/Module/Modules/Combat/Hitbox.h
@@ -0,0 +1,19 @@
+#ifndef CLIENT_ARES_CATEGORY_MODULE_MODULES_COMBAT_HITBOX
+#define CLIENT_ARES_CATEGORY_MODULE_MODULES_COMBAT_HITBOX
+
+#include "../../Module.h"
+
+class Hitbox : public Module {
+public:
+    Hitbox(Category* c) : Module("Hitbox", c) {
+        //this->isEnabled = true;
+    };
+public:
+    auto onGameMode(GameMode*) -> void override;
+    auto onDisable(void) -> void override;
+public:
+    Vec2<float> radius = Vec2<float>(2.f, 2.f);
+    std::map<uint8_t, Vec2<float>> collisions;
+};
+
+#endif /* CLIENT_ARES_CATEGORY_MODULE_MODULES_COMBAT_HITBOX */
