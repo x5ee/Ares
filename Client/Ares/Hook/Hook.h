c752617b8a9b6b4c94340751b09aabfc29b42632
OvermindMC <Overmind@mail.com>
2023-07-18 05:39:44 +0100

    Update Hook.h

--- Client/Ares/Hook/Hook.h
+++ Client/Ares/Hook/Hook.h
@@ -1,7 +1,14 @@
 #ifndef CLIENT_ARES_HOOK_HOOK
 #define CLIENT_ARES_HOOK_HOOK
 
+#include "../../SDK/Minecraft.h"
+#include "../Manager.h"
+
 class Hook {
+public:
+    Manager* mgr = nullptr;
+public:
+    Hook(Manager*);
 public:
     virtual auto init(void) -> void {};
 };
