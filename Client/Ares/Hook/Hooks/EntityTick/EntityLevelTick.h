0fe89461437224bacabe03fcf87a86d339a86d6a
OvermindMC <Overmind@mail.com>
2023-07-18 05:39:33 +0100

    Create EntityLevelTick.h

---
+++ Client/Ares/Hook/Hooks/EntityTick/EntityLevelTick.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_ARES_HOOK_HOOKS_ENTITYTICK_ENTITYLEVELTICK
+#define CLIENT_ARES_HOOK_HOOKS_ENTITYTICK_ENTITYLEVELTICK
+
+#include "../../Hook.h"
+
+class EntityLevelTickHook : public Hook {
+public:
+    EntityLevelTickHook(Manager* mgr) : Hook(mgr) {
+        this->init();
+    };
+public:
+    auto init(void) -> void override;
+};
+
+#endif /* CLIENT_ARES_HOOK_HOOKS_ENTITYTICK_ENTITYLEVELTICK */
