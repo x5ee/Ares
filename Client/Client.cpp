774fcdf202ca59513ba24d44c5a6e7e50441f661
OvermindMC <Overmind@mail.com>
2023-07-18 08:13:19 +0100

    Update Client.cpp

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -4,8 +4,16 @@
 Client::Client(std::string n) {
 
     Utils::setDebugPath(n);
-    
+    this->isRunning = true;
+
     this->name = n.c_str();
     this->mgr = new Manager(this);
+
+    while(this->isRunning) {
+        
+        this->mgr->baseTick();
+        Sleep(1);
+
+    };
 
 };
