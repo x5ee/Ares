111493c55d30d83f92364ef42b0a0ba286eadfe0
OvermindMC <Overmind@mail.com>
2023-07-18 14:58:15 +0100

    Update TestModule.cpp

--- Client/Ares/Category/Module/Modules/Other/TestModule.cpp
+++ Client/Ares/Category/Module/Modules/Other/TestModule.cpp
@@ -1,5 +1,15 @@
 #include "TestModule.h"
 
+auto TestModule::onImGui(void) -> void {
+
+    if(ImGui::Begin("ImGui")) {
+
+        ImGui::End();
+
+    };
+
+};
+
 auto TestModule::onGameMode(GameMode* GM) -> void {
 
     GM->player->setSprinting(true);
