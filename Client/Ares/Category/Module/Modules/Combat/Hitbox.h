e4424abaa37e4ed12f36b519056389c852ce2eb8
OvermindMC <Overmind@mail.com>
2023-07-20 17:34:34 +0100

    Update Hitbox.h

--- Client/Ares/Category/Module/Modules/Combat/Hitbox.h
+++ Client/Ares/Category/Module/Modules/Combat/Hitbox.h
@@ -12,6 +12,8 @@ public:
     auto onGameMode(GameMode*) -> void override;
     auto onDisable(void) -> void override;
 public:
+    auto onImGuiOptions(void) -> void override;
+public:
     Vec2<float> radius = Vec2<float>(2.f, 2.f);
     std::map<uint8_t, Vec2<float>> collisions;
 };
