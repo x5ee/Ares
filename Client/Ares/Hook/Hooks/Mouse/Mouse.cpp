c96ca9b34a176994f78e8e87879168f6a94663a3
OvermindMC <Overmind@mail.com>
2023-07-22 02:45:05 +0100

    Update Mouse.cpp

--- Client/Ares/Hook/Hooks/Mouse/Mouse.cpp
+++ Client/Ares/Hook/Hooks/Mouse/Mouse.cpp
@@ -1,4 +1,5 @@
 #include "Mouse.h"
+#include "../../../Category/Module/Module.h"
 
 typedef void(__thiscall* Mouse)(void*, MouseAction, bool, short, short, void*, void*, void*);
 Mouse oMouse;
@@ -7,10 +8,24 @@ Manager* mhMgr = nullptr;
 
 auto MouseHookCallback(void* p1, MouseAction action, bool isDown, short p4, short p5, void* p6, void* p7, void* p8) -> void {
 
+    bool cancel = false;
+
 	if (ImGui::GetCurrentContext() != nullptr && (action != MouseAction::NONE && action != MouseAction::WHEEL_SCROLL && action != MouseAction::WHEEL_CLICK))
 		ImGui::GetIO().MouseDown[0] = isDown;
+    
+    for(auto category : mhMgr->categories) {
 
-	oMouse(p1, action, isDown, p4, p5, p6, p7, p8);
+        for(auto module : category->modules) {
+            
+            if(module->isEnabled)
+                module->onMouse(action, isDown, Vec2<short>(p4, p5), &cancel);
+
+        };
+
+    };
+
+	if(!cancel)
+        oMouse(p1, action, isDown, p4, p5, p6, p7, p8);
 
 };
 
