b7b134b575bab50a409164d573a93dcfd4dfae30
OvermindMC <Overmind@mail.com>
2023-07-22 03:07:23 +0100

    Update TestModule.cpp

--- Client/Ares/Category/Module/Modules/Other/TestModule.cpp
+++ Client/Ares/Category/Module/Modules/Other/TestModule.cpp
@@ -8,7 +8,13 @@ auto TestModule::onTick(void) -> void {
 
 auto TestModule::onEnable(void) -> void {
 
-    //
+    auto instance = MC::getClientInstance();
+    auto player = (instance ? instance->getPlayer() : nullptr);
+
+    std::ostringstream o;
+    o << std::hex << player;
+
+    Utils::debugOutput(std::string("Player: " + o.str()));
 
 };
 
