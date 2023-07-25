b07b71df8ec96a06eb14eeb3ddc3e092e522380a
OvermindMC <Overmind@mail.com>
2023-07-25 10:20:02 +0100

    Update MovementProxy.cpp

--- Client/SDK/Classes/MovementProxy.cpp
+++ Client/SDK/Classes/MovementProxy.cpp
@@ -26,3 +26,21 @@ auto MovementProxy::getHealth(void) -> int {
     return _GetHealth(this);
 
 };
+
+auto MovementProxy::setCollision(bool state) -> void {
+
+    using SetCollision = void (__thiscall*)(MovementProxy*, bool);
+    auto _SetCollision = (SetCollision)(this->VTable[48]);
+
+    _SetCollision(this, state);
+
+};
+
+auto MovementProxy::setRotation(Vec2<float>* rot) -> void {
+
+    using SetRotation = void (__thiscall*)(MovementProxy*, Vec2<float>*);
+    auto _SetRotation = (SetRotation)(this->VTable[74]);
+
+    _SetRotation(this, rot);
+
+};
