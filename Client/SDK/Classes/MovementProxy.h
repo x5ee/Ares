e2b7b0702a5a5823be28ffea2c0db9bed1819a5b
OvermindMC <Overmind@mail.com>
2023-07-25 07:19:17 +0100

    Create MovementProxy.h

---
+++ Client/SDK/Classes/MovementProxy.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT_SDK_CLASSES_MOVEMENTPROXY
+#define CLIENT_SDK_CLASSES_MOVEMENTPROXY
+
+#include "../../Utils/Utils.h"
+
+class MovementProxy {
+public:
+    uintptr_t** VTable;
+public:
+    auto isOnGround(void) -> bool;
+    auto setOnGround(bool) -> void;
+};
+
+#endif /* CLIENT_SDK_CLASSES_MOVEMENTPROXY */
